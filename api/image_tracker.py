eef842f6f6c48349322b239ea2c50e4ef5e86c87
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-29 15:19:24 +0200

    Image management

--- api/image_tracker.py
+++ api/image_tracker.py
@@ -4,7 +4,7 @@ from deep_translator import GoogleTranslator
 def get_image_url(item):
 
   query = GoogleTranslator(source='auto', target='en').translate(item)
-  apiKey = "o46LtQkQP1Nwucqdqdel-jED6rUNX7mHv5eGfpl0w24"
+  apiKey = ""
   url = f'https://api.unsplash.com/photos/random?query={query}&client_id={apiKey}'
   response = requests.get(url)
   if response.status_code == 200:

1a030f0df46e62a8a26466f66a8aab3745dcc002
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-29 16:17:58 +0200

    React Native client base

--- api/image_tracker.py
+++ api/image_tracker.py
@@ -4,7 +4,7 @@ from deep_translator import GoogleTranslator
 def get_image_url(item):
 
   query = GoogleTranslator(source='auto', target='en').translate(item)
-  apiKey = ""
+  apiKey = "o46LtQkQP1Nwucqdqdel-jED6rUNX7mHv5eGfpl0w24"
   url = f'https://api.unsplash.com/photos/random?query={query}&client_id={apiKey}'
   response = requests.get(url)
   if response.status_code == 200:

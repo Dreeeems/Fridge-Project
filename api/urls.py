ade22949eba9fa1e81b4723e369127d94d4d1c00
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-29 12:53:10 +0200

    basic crud with rest_framework

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path 
-from .views import  ItemList,ItemDetail,ItemCreate,ItemUpdate,ItemDelete,TypeList,TypeDetail,TypeCreate,TypeUpdate,TypeDelete
+from .views import  ItemList,ItemDetail, TypeList,TypeDetail
 urlpatterns = [
  path('items/',ItemList.as_view()),
  path('item/<int:pk>',ItemDetail.as_view()),
- path('item/create',ItemCreate.as_view()),
- path('item/<int:pk>/update',ItemUpdate.as_view()),
- path('item/<int:pk>/delete',ItemDelete.as_view()),
- path('types/',TypeList.as_view()),
+ path('type/',TypeList.as_view()),
  path('type/<int:pk>',TypeDetail.as_view()),
- path('type/create',TypeCreate.as_view()),
- path('type/<int:pk>/update',TypeUpdate.as_view()),
- path('type/<int:pk>/delete',TypeDelete.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item, Type
+from .serializers import ItemSerializer, typeSerializer
 
-# Create your views here.
+
+#Get and Post route  for item
+class ItemList(generics.ListCreateAPIView):
+        queryset = Item.objects.all()
+        serializer_class= ItemSerializer
+
+
+#Get,Patch and Delete route for item with is pk key 
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+        queryset = Item.objects.all()
+        serializer_class = ItemSerializer
+
+
+#Get and Post route  for type
+
+class TypeList(generics.ListCreateAPIView):
+        queryset= Type.objects.all()
+        serializer_class = typeSerializer
+
+#Get,Patch and Delete route for type with is pk key 
+class TypeDetail(generics.RetrieveUpdateDestroyAPIView):
+        queryset = Type.objects.all()
+        serializer_class = typeSerializer

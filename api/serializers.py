1feac05a37b5bd27f419c583d490341e218260cd
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-09-29 11:41:01 +0200

    Item Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,10 @@ class typeSerializer(serializers.ModelSerializer) :
     class Meta:
         model = Type
         fields = ['id','name']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    type = typeSerializer()
+    class Meta : 
+        model = Item
+        fields = ['id','name','type','number','lastDate']
